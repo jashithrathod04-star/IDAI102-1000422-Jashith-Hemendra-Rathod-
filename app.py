b8b2555500280e2ae1598dc91272b3a2dee2d212
jashithrathod04-star <jashith.rathod04@gmail.com>
2026-01-27 01:21:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def apply_theme():
         }}
         
         .stButton > button:hover {{
-            transform: scale(2);
+            transform: scale(1.5);
             box-shadow: 0 6px 14px rgba(0,0,0,0.15);
         }}
 
