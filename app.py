6083ba8c705ceda302444f39b60363ddd58f1811
jashithrathod04-star <jashith.rathod04@gmail.com>
2026-01-26 16:13:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def apply_theme():
         }}
 
         div[data-testid="metric-container"] {{
-            background-color: #C9F7D5;
+            background-color: #e4f5e8;
             border-radius: 14px;
             padding: 14px;
             border-left: 6px solid {accent};
